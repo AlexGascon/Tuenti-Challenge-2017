4eb2d17dc327d2e2bef1534f93ed1c6fbe1370f5
Alex <alexgascon.93@gmail.com>
2017-06-06 11:44:20 +0200

    Challenge 1 + Challenge 2 - Comments + improved readability

--- Challenge 1/challenge1.py
+++ Challenge 1/challenge1.py
@@ -1,5 +1,53 @@
+# coding=utf-8
+"""
+CHALLENGE 1: PIZZA LOVE (https://contest.tuenti.net/Challenges?id=1)
+
+We love pizza. You love pizza. And your friends love pizza too. You are hosting a party tomorrow, and you want to have
+enough pizza for everyone. Following the widely-known standards, every pizza is cut into 8 slices, and you know the
+maximum number of pizza slices that each person will eat. You want to know the minimum number of pizzas you need to
+order so that nobody goes hungry during the party.
+
+INPUT
+In the first line, an integer T indicates the number of cases.
+Each case consists of two lines. The first one contains a number N, the number of people attending the party.
+The second line contains N numbers, representing the maximum number of pizza slices that each guest eats (S).
+
+OUTPUT
+For each case, a line starting with "Case #x: " followed by the minimum number of pizzas you need to order to ensure
+that nobody goes hungry.
+
+LIMITS
+    1 ≤ T ≤ 100
+    1 ≤ N ≤ 10000
+    1 ≤ S ≤ 100
+
+SAMPLE INPUT
+3
+3
+8 8 8
+2
+5 3
+4
+3 4 5 6
+
+SAMPLE OUTPUT
+Case #1: 3
+Case #2: 1
+Case #3: 3
+
+In the first case, each of the three attendees eats an entire pizza, so the answer is 3.
+In the second case, the two attendees eat 8 slices, so a single pizza is enough for them.
+In the third case, the answer is 3:
+
+    Attendees #1 and #3 eat an entire pizza together (3+5=8)
+    Attendee #2 eats 4 slices of a second pizza
+    Attendee #4 eats up to 6 pizza slices, but only 4 remain from the second pizza, so we need another one.
+
+"""
+
 import math
 
+# Preparing the input
 fin = open('submitInput.txt')
 fout = open('submitOutput.txt', 'w')
 
@@ -7,10 +55,12 @@ lines = fin.readlines()
 cases = int(lines[0])
 
 for i in range(1, cases+1):
-	slices = lines[2*i]
-	total_slices = sum([int(pizza_slices) for pizza_slices in slices.split(" ")])
-	pizzas = int(math.ceil(total_slices/8.0))
+    # Obtaining the slices for this case and counting them
+    slices = lines[2*i]
+    total_slices = sum([int(pizza_slices) for pizza_slices in slices.split(" ")])
+    # Pizzas needed: slices/8 rounded up.
+    pizzas = int(math.ceil(total_slices/8.0))
 
-	fout.write('Case #{}: {}\n'.format(i, pizzas))
+    fout.write('Case #{}: {}\n'.format(i, pizzas))
 
 
--- Challenge 2/challenge2.py
+++ Challenge 2/challenge2.py
@@ -1,37 +1,116 @@
+"""
+CHALLENGE 2 - BOWLING (https://contest.tuenti.net/Challenges?id=2)
+
+You have probably heard about bowling before, but you may not know how to score a bowling game correctly.
+A game of bowling consists of ten frames, in which the player has the chance to knock down ten pins. The player has two
+rolls in each frame, which means he can throw the ball twice. If the player knocks down all ten pins on the first roll,
+they don't need a second roll for the frame. This kind of frame is called a "strike". If the player knocks down some
+pins on the first roll and all remaining pins on the second roll (i.e.: 6 and 4), we call the frame a "spare". For each
+frame, the player is awarded as many points as pins knocked down. For instance, if the player knocks down 3 and 5 pins
+the score awarded for the frame is 8. If the frame is a spare, the pins knocked down on the next roll are awarded as a
+bonus. If the frame is a strike, the pins knocked down on the next two rolls are awarded as a bonus. The 10th frame is
+a bit special: if it ends up being spare or a strike, the player gets one or two extra rolls. These extra rolls are
+solely used to add extra points to the strike/spare and don't provide any points by themselves. Take a look here
+(http://slocums.homestead.com/gamescore.html) for more information.
+
+You have been asked to program a simple firmware for your local bowling alley. You need to calculate the score of each
+frame based on the number of pins knocked down on each roll of a game.
+
+
+INPUT
+The first line will contain an integer C, the number of cases for our problem.
+Each case starts with a line with an integer R, the number of rolls. A line with R integers follows, each indicating the
+pins knocked down in the roll Ri.
+
+
+OUTPUT
+For each case, a line starting with "Case #x: " followed by 10 integers separated by spaces, indicating the score of
+each frame. Every line is followed by a new line character.
+
+
+EXAMPLES
+Case 1:
+20
+5 4 1 3 0 7 4 5 3 0 2 5 2 4 5 0 7 1 4 4
+
+Case 2:
+19
+9 0 4 0 8 0 4 5 7 0 10 4 1 7 1 6 3 5 4
+
+Case 3:
+17
+10 7 2 9 1 10 10 7 1 8 0 7 0 10 10 4 2
+
+Case 4:
+12
+10 10 10 10 10 10 10 10 10 10 10 10
+
+Case 1 is very straightforward as there aren't any strikes or spares.
+In Case 2, there is a strike in the 6th frame, which gets 10 + 4 + 1 points.
+In Case 3, there are a few strikes and spares. Pay special attention to the last frame: it gets 10 + 4 + 2 points.
+In Case 4 is the perfect bowling game: every frame is worth 30 points.
+
+
+SAMPLE INPUT
+4
+20
+5 4 1 3 0 7 4 5 3 0 2 5 2 4 5 0 7 1 4 4
+19
+9 0 4 0 8 0 4 5 7 0 10 4 1 7 1 6 3 5 4
+17
+10 7 2 9 1 10 10 7 1 8 0 7 0 10 10 4 2
+12
+10 10 10 10 10 10 10 10 10 10 10 10
+
+
+SAMPLE OUTPUT
+Case #1: 9 13 20 29 32 39 45 50 58 66
+Case #2: 9 13 21 30 37 52 57 65 74 83
+Case #3: 19 28 48 75 93 101 109 116 140 156
+Case #4: 30 60 90 120 150 180 210 240 270 300
+
+"""
+
+# Preparing the input
 fin = open('submitInput.txt')
 fout = open('submitOutput.txt', 'w')
 
 lines = fin.readlines()
 cases = int(lines[0])
+
 for i in range(1, cases+1):
     fout.write('Case #{}:'.format(i))
 
+    # Pins knocked down in each roll
     rolls = [int(roll) for roll in lines[2*i].split(" ")]
 
-    score = 0
+    score = 0  # Total score achieved on this play
+    j = 0   # Indicates the roll we're currently counting
+    current_frame = 0  # Current frame
 
-    j = 0
-    num_rolls = 0
     while True:
 
+        # STRIKE: Adding the current frame and the two following rolls
         if rolls[j] == 10:
-            score = score + rolls[j] + rolls[j+1] + rolls[j+2]
-            j += 1
-            num_rolls += 1
+            score += rolls[j] + rolls[j+1] + rolls[j+2]
+            j += 1  # The next roll is part of a new frame, so we'll need to count it again
 
+        # SPARE: Adding the current frame and the next roll
         elif (rolls[j] + rolls[j+1] == 10):
-            score = score + rolls[j] + rolls[j+1] + rolls[j+2]
-            j += 2
-            num_rolls += 1
+            score += rolls[j] + rolls[j+1] + rolls[j+2]
+            j += 2  # The next roll is part of the current frame, so we'll skip it
 
+        # ELSE: Adding the current frame
         else:
-            score = score +  rolls[j] + rolls[j+1]
-            j += 2
-            num_rolls += 1
+            score +=  rolls[j] + rolls[j+1]
+            j += 2  # The next roll is part of the current frame, so we'll skip it
+
+        current_frame += 1
 
         fout.write(' {}'.format(score))
 
-        if j >= len(rolls) or num_rolls == 10:
+        # Stop condition: no more rolls left or we've already completed frame 10.
+        if j >= len(rolls) or current_frame == 10:
             break
 
 
